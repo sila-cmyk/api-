e11b5242e25c37ce30989c5bb9101a224764492c
sila-cmyk <slazorlu224@gmail.com>
2025-12-25 00:20:47 +0300

    Update planet.js

--- services/planet.js
+++ services/planet.js
@@ -1,18 +1,16 @@
 export async function getPlanetData(planetName) {
-    // Boşlukları temizle ve küçük harfe çevir
     const cleanName = planetName.trim().toLowerCase();
-    
-    // Güneş sistemi API'si genellikle İngilizce isimleri kabul eder
+    // Güneş sistemi API'si
     const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${cleanName}`);
     
     if (!response.ok) {
-        throw new Error(`'${planetName}' gezegeni sistemde kayıtlı değil.`);
+        throw new Error("Gezegen adı bulunamadı. Lütfen İngilizce yazın (Örn: Mars, Jupiter, Saturn)");
     }
     
     const data = await response.json();
     
     return {
-        name: data.englishName || data.name,
-        gravity: data.gravity
+        name: data.englishName,
+        gravity: data.gravity // API'den gelen yerçekimi değeri (m/s²)
     };
 }
