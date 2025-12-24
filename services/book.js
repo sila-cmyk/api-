3b50ef1ddc697e34cc6b05c510ddc547eb0250b4
sila-cmyk <slazorlu224@gmail.com>
2025-12-24 21:41:23 +0300

    Update and rename services/book.js to planet.js

--- services/book.js
+++
@@ -1,8 +0,0 @@
-export function getBook(isbn) {
-  const books = {
-    "123": { title: "Kürk Mantolu Madonna", pages: 160 },
-    "456": { title: "Saatleri Ayarlama Enstitüsü", pages: 300 }
-  };
-
-  return books[isbn];
-}
